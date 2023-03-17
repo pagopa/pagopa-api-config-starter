fdf8b5b359eb6676cbf284d3a2ec91057357e6b5
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-03-17 09:12:13 +0100

    google java format

--- src/main/java/it/gov/pagopa/apiconfig/starter/repository/CanaliRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/starter/repository/CanaliRepository.java
@@ -4,7 +4,6 @@ import it.gov.pagopa.apiconfig.starter.entity.Canali;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @SuppressWarnings("java:S100")
