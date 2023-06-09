f83c557f03b12a1c4529ff4b659138657258b2af
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-06-09 09:41:44 +0200

    PAGOPA-896 using optional in iban

--- src/main/java/it/gov/pagopa/apiconfig/starter/repository/IbanRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/starter/repository/IbanRepository.java
@@ -3,9 +3,10 @@ package it.gov.pagopa.apiconfig.starter.repository;
 import it.gov.pagopa.apiconfig.starter.entity.Iban;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface IbanRepository extends JpaRepository<Iban, Long> {
 
-  Iban findByIban(String iban);
+  Optional<Iban> findByIban(String iban);
 }
