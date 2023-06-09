da92e5e0e5c07a8ffeb5c7f622c3b81273a00fd6
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-06-09 10:09:53 +0200

    PAGOPA-896 adding iban search to ibanMaster

--- src/main/java/it/gov/pagopa/apiconfig/starter/repository/IbanMasterRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/starter/repository/IbanMasterRepository.java
@@ -3,8 +3,10 @@ package it.gov.pagopa.apiconfig.starter.repository;
 import it.gov.pagopa.apiconfig.starter.entity.IbanMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IbanMasterRepository extends JpaRepository<IbanMaster, Long> {
 
+  List<IbanMaster> findByIban(Long fkIban);
 }
