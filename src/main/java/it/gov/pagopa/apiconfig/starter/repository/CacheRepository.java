115dbff45c3ca64191efa785986c57bfdb683504
Lorenzo Catalano <lorenzo.catalano@tasgroup.eu>
2023-11-07 10:35:12 +0100

    flag_psp_cp -> flag_travaso

--- src/main/java/it/gov/pagopa/apiconfig/starter/repository/CacheRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/starter/repository/CacheRepository.java
@@ -1,15 +1,14 @@
 package it.gov.pagopa.apiconfig.starter.repository;
 
 import it.gov.pagopa.apiconfig.starter.entity.Cache;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CacheRepository extends PagingAndSortingRepository<Cache, Long> {
@@ -20,5 +19,6 @@ public interface CacheRepository extends PagingAndSortingRepository<Cache, Long>
   List<Cache> findAll();
 
   Page<Cache> findAll(Pageable pageable);
+
   Page<Cache> findByVersionNotLike(String versionLike, Pageable pageable);
 }
