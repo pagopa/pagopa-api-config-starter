f5428eeee32d48ef9938d20048da18f4d2f210e4
aacitelli <aacitelli@host.docker.internal>
2023-12-14 10:04:06 +0100

    [PAGOPA-1378] sviluppo caricamento iban da csv: jpa persistence management

--- src/main/java/it/gov/pagopa/apiconfig/starter/repository/IbanAttributeMasterRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/starter/repository/IbanAttributeMasterRepository.java
@@ -3,10 +3,16 @@ package it.gov.pagopa.apiconfig.starter.repository;
 import it.gov.pagopa.apiconfig.starter.entity.IbanAttributeMaster;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IbanAttributeMasterRepository extends JpaRepository<IbanAttributeMaster, Long> {
 
   public List<IbanAttributeMaster> findByFkIbanMasterIn(List<Long> fkIbanMaster);
+  
+  @Modifying
+  @Query("delete from IbanAttributeMaster iam where iam.objId in ?1")
+  void deleteByIds(List<Long> ids);
 }
