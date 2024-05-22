bc0fcf2ea4797b9d1dce367a801cb16ec613b2d7
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-05-22 15:02:17 +0200

    PAGOPA-1769 adding query in ibanMaster and Pa, update Java version

--- src/main/java/it/gov/pagopa/apiconfig/starter/repository/PaRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/starter/repository/PaRepository.java
@@ -4,6 +4,7 @@ import it.gov.pagopa.apiconfig.starter.entity.Pa;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +13,7 @@ public interface PaRepository extends JpaRepository<Pa, Long> {
   Optional<Pa> findByIdDominio(String creditorInstitutionCode);
 
   Optional<List<Pa>> findByIdDominioIn(List<String> creditorInstitutionCodes);
+
+  @Query(value = "SELECT pa FROM Pa pa WHERE pa.cbill IS NULL OR pa.cbill = ''")
+  Optional<List<Pa>> findPaWithoutCbill();
 }
